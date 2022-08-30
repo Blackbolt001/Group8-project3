01b0d388fd2c6af3a865974f473bf0550bffb000
unknown <Jgossett73@gmail.com>
2022-08-30 14:08:59 -0400

    refractoring server side

---
+++ server/utils/auth.js
@@ -0,0 +1,35 @@
+const jwt = require('jsonwebtoken');
+const secret = 'mysecretsshhhhh';
+const expiration = '2h';
+
+module.exports = {
+  // function for our authenticated routes
+  authMiddleware: function ({req}) {
+    let token = req.body.token || req.query.token || req.headers.authorization;
+   // ["Bearer", ,tokenvalue>]
+    if (req.headers.authorization) {
+      token = token.split(' ').pop().trim();
+    }
+
+    if (!token) {
+      return req;
+    }
+    try {
+      const { data } = jwt.verify(token, secret, { maxAge: expiration });
+      req.user = data;
+    } catch {
+      console.log('Invalid token');
+      return req.status(400).json({ message: 'invalid token!' });
+    }
+    return req;
+
+   },
+    signToken: function ({ username, email, _id }) {
+    const payload = { username, email, _id };
+    const token = jwt.sign({data:payload}, secret, {
+      expiresIn:expiration,
+     });
+    return token;
+    },
+  };
+  
