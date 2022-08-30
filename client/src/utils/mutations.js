8b093c571611e2242c11a49e25fbd28ebfb9b1bb
cwrightj <cwrightj@umich.edu>
2022-08-30 13:32:13 -0400

    check

--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -14,6 +14,19 @@ export const CREATE_OWNER = gql`
   }
 `;
 
+export const LOGIN_USER = gql`
+  mutation createOwner($name: String!, $username: String!,$password: String!, $age: Int, $interests: String!, $pet: [Pet]) {
+    createMatchup(name: $name, username: $username,password: $password, age: $age, interests: $interests, pet: $pet) {
+      _id
+      name
+      username
+      password
+      age
+      interests
+      pet
+    }
+  }
+`;
 export const CREATE_PET = gql`
   mutation createVote($petname: String!,$breed: String!, $age: Int, $nature: String!, $Gender: String!) {
     createVote(petname:$petname, breed: $breed, age: $age, nature:$nature, gender:$ge) {
--- server/server.js
+++ server/server.js
@@ -5,7 +5,7 @@ const path = require('path');
 const { typeDefs, resolvers } = require('./schemas');
 const db = require('./config/connection');
 
-const PORT = process.env.PORT || 3007;
+const PORT = process.env.PORT || 3012;
 const app = express();
 const server = new ApolloServer({
   typeDefs,
