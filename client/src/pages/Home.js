ea722d94187c80e454b9efacfbf88cdc18d6f429
Tanner Shadday <tshadday99@gmail.com>
2022-09-01 17:11:23 -0400

    merge

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,15 +1,27 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Navigate } from "react-router-dom";
 import Auth from '../utils/auth';
 import SwipeCard from '../components/SwipeCard'
+import Info from '../components/info'
 
 const Home = () => {
-console.log("seeing home")
+
+    // useState to hold visibility
+    const [isVisible, setIsVisible] = useState(true);
+
+    // function to change visibility
+    const showInfo = event => {
+        setIsVisible(current => !current);
+    };
+
     return(
         <div> 
             <div>
-             <SwipeCard/>
-                    </div>
+                <SwipeCard/>
+                <div className={`editPetForm ${isVisible ? 'hidden' : 'visible'}`}>
+                    <Info />
+                </div>
+            </div>
             {Auth.loggedIn()
                 // Displays page info if logged in
                 ? (
